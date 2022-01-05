ea94652ee7bab7d73fbe5da36c1e11d8d0fc705e
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-01-05 15:01:33 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/global.scss';
+
+console.log('hola');
+
+ReactDOM.render( < App / > , document.getElementById('app'));
